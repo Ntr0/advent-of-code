74d2dd36a35f9e7635a041f26084c57ca2e47613
Christian Schulze-Wiehenbrauk <christian.schulze-wiehenbrauk@ionos.com>
2022-12-14 11:11:21 +0100

    Corrected, so second half worked as well

--- 2022/day6/part1.sh
+++ 2022/day6/part1.sh
@@ -10,40 +10,39 @@ sqlite-utils memory input.tsv "
 		union all 
 		select 
 			substr(rest,1,1), 
-			length(substr(rest,2)), 
+			(select length(data) from input where id == 1) - length(substr(rest,2)),
 			substr(rest,2) 
 		from split 
 		where rest <> '' 
 	), 
-	positions(char, pos) as (
+	positions(char, pos, distance) as (
 		select 
-			a.char, a.pos 
-		from split as a 
-		where 
-			a.pos - $length > (
-	       			select 
-					max(b.pos) 
-				from split as b 
-				where a.char = b.char and a.pos > b.pos
-			) or 
-			0 = ( 
-				select count(*) 
-				from split b 
-				where b.char = a.char and b.pos < a.pos
-			) 
-	)  
-	select min(startpos)
-	from (
-		select 
+			a.char, 
 			a.pos, 
-			count(*) as cnt, 
-			length(input.data) - a.pos - 1 + count(*) as startpos 
-		from positions as a 
-		join positions b on 
-			a.pos - $length < b.pos and 
-			a.pos >= b.pos and 
-			b.char <> '' 
-		join input on ID == 1 
-		group by a.pos 
-		) 
-	where cnt == $length;" 
+			b.pos - a.pos
+		from split as a 
+		join split as b 
+		on
+			a.char = b.char
+		where 	
+		b.pos = (select min(c.pos) from split as c where c.pos > a.pos and c.char == a.char)
+		)
+		select min(startpos)
+    from (
+      select
+        a.pos,
+        count(*) as cnt,
+        a.distance,
+        a.pos + $length - 1 as startpos
+      from positions as a
+      join positions b on
+        a.pos + $length > b.pos and
+        -- mnlblmnlb 3 & 2 -> 2 - (3 - 1 - 1)
+        b.distance + b.pos > a.pos + $length - 1 and
+        a.pos <= b.pos and
+        b.char <> ''
+      where a.distance >= $length
+      group by a.pos
+    )
+    where cnt == $length
+"
